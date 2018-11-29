d88998b5e0f095475ef7cadc4390a79bf02ccfe4
Debbie Ellis <debbieellis7@gmail.com>
2018-11-29 22:05:59 +0800

    Prepared for deploy

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,5 @@
-module.exports = {
-  mongoURI: 'mongodb://userapp:asdasd1@ds117592.mlab.com:17592/devconnector'
-};
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
+}
